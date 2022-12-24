e30c0d4f4c6eb1f4dd2cf97273198b98326f5116
Rubinashabbir1999 <rubinashabbir1999@gmail.com>
2022-12-24 19:35:01 +0500

    created clock using python

---
+++ learn.py
@@ -0,0 +1,11 @@
+def hello(name):
+    if name == 'bob':
+        return print("hello bob!!")
+    else:
+        return print("hello : " + name)
+    
+
+
+
+nam = input("enter your name : ")
+hello(nam)
